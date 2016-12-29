78cc4c12b5dd3e219d5ca1ce2b22480e28a32283
hi618102 <hi1002@aliyun.com>
2016-12-29 11:03:57 +0000

    增加Swagger2

--- Vacomall-Spring-RestApi/src/main/java/com/vacomall/common/anno/TokenSecurity.java
+++ Vacomall-Spring-RestApi/src/main/java/com/vacomall/common/anno/TokenSecurity.java
@@ -14,6 +14,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface IgnoreSecurity {
+public @interface TokenSecurity {
 
 }
