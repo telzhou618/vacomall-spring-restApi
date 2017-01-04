2b883a0f9694da18883386a1702264e75906f827
gaojun.zhou <274410087@qq.com@9572bc91-7102-41d3-95d4-ae011026e002>
2017-01-04 12:21:35 +0000

    提高代码质量

--- Vacomall-Spring-RestApi/src/main/java/com/vacomall/service/impl/SysUserServiceImpl.java
+++ Vacomall-Spring-RestApi/src/main/java/com/vacomall/service/impl/SysUserServiceImpl.java
@@ -2,10 +2,10 @@ package com.vacomall.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
-import com.vacomall.common.util.MD5;
 import com.vacomall.entity.SysUser;
 import com.vacomall.mapper.SysUserMapper;
 import com.vacomall.service.ISysUserService;
@@ -23,6 +23,7 @@ public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> impl
 	@Override
 	public SysUser login(SysUser sysUser) {
 		// TODO Auto-generated method stub
-		return this.selectOne(new EntityWrapper<SysUser>().eq("userName", sysUser.getUserName()).eq("password", MD5.toMD5(sysUser.getPassword())));
+		String password = DigestUtils.md5DigestAsHex(sysUser.getPassword().getBytes());
+		return this.selectOne(new EntityWrapper<SysUser>().eq("userName", sysUser.getUserName()).eq("password", password));
 	}
 }
